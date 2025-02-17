dca57053228973218feb607a5ce73dd3db57aa50
Gautam <gautamratanpuri1311@gmail.com>
2025-02-18 00:08:04 +0530

    added modularity to the game params

--- guess-the-number.py
+++ guess-the-number.py
@@ -1,10 +1,15 @@
+# GUESSING GAME
 import random
+import math
 
 print('Welcome.\nThis is a guessing game.\nYou will get 7 chances to guess a random number between 1 and 100 chosen by the compiler :)')
+range_lowlimit = int(input("Enter range lower limit: "))
+range_uplimit = int(input("Enter range upper limit: "))
+chosen_num = random.randrange(range_lowlimit,range_uplimit)
 
-chosen_num = random.randrange(1,100)
-# print(chosen_num)
-guess_counter = 7
+# Minimum number of guessing = log2(Upper bound – lower bound + 1)
+log_input = range_uplimit - range_lowlimit + 1
+guess_counter = math.ceil(math.log(log_input,2))
 
 while (guess_counter > 0):
     print(f'You have {guess_counter} guesses left.')
